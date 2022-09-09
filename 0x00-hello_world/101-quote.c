be4c4ae9639bc9d2c256f3163dd6ee12ae157d07
Mia Ikechukwu <mira.ikechukwu15@gmail.com>
2022-09-09 07:46:47 -0700

    Debugged last commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <unistd.h>
+#include<unistd.h>
 
 /**
- *main - main entry point
+ *main - Prints the standard error sandwiched around the standard output
  *
  *Return: returns 1
  */
 int main(void)
 {
-write(2, "and that piece of art is useful \" - Dora Korpar, 2015-10-19 \n", 59);
+write(2, "and that piece of art is useful\"- Dora Korpar,2015-10-19\n", 59);
 return (1);
 }
