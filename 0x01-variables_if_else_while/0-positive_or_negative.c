d6e54a3659117ea8d0ba1fffcaa4c3002e678503
Mia Ikechukwu <mira.ikechukwu15@gmail.com>
2022-09-09 19:53:31 -0700

    Added a new program

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d is positve\n", n);
+printf("%d is positive\n", n);
 }
 else if (n < 0)
 {
@@ -23,7 +23,7 @@ printf("%d is negative\n", n);
 }
 else
 {
-printf("%d is zero", n);
+printf("%d is zero\n", n);
 }
 return (0);
 }
