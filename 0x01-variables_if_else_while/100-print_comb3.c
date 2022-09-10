3ac14822137be6f90c284938a1044ba1ff420ece
Mia Ikechukwu <mira.ikechukwu15@gmail.com>
2022-09-10 07:41:10 -0700

    Uploaded new program

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - prints numbers with space and comma inbetween
+ *
+ *Return: 0
+ */
+int main(void)
+{
+int a;
+int b;
+for (a = 0; a < 9; ++a)
+{
+for (b = a + 1; b < 10; ++b)
+{
+putchar((a % 10) + '0');
+putchar((b % 10) + '0');
+if (a == 8 && b == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
