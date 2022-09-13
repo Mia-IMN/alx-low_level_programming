6985364ec8ac6ac729b45e5db66a1bb6b3ad555d
Mia Ikechukwu <mira.ikechukwu15@gmail.com>
2022-09-13 15:41:28 -0700

    Added a program that checks for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include <stdio.h> 
+#include <stdbool.h>
+
+/**
+*_islower - checks for lowercases
+*
+*Return: 0
+*/
+int _islower(int c)
+{
+if (c > 96 && c < 123)
+{
+return (1);
+}
+else
+return (0);
+}
