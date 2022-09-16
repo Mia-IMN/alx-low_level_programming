7a880ee7b017b20145b20a443f8ec41d6431353a
Mia Ikechukwu <mira.ikechukwu15@gmail.com>
2022-09-15 17:10:05 -0700

    Added new program file?

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ *_isupper - checks for uppercase lowercase
+ *@c: int
+ *Return: 1
+ */
+int _isupper(int c)
+{
+if (isupper(c))
+return (1);
+else
+return (0);
+}
