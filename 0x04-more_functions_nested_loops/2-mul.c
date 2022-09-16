959dfb0304fd3e8302aa684298d61b7e63be46d0
Mia Ikechukwu <mira.ikechukwu15@gmail.com>
2022-09-15 17:25:47 -0700

    Added new multiplying program file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *main - multiplies two integers
+ *
+ *Return: 0
+ */
+int mul(int a, int b)
+{
+int xtimes;
+xtimes = a * b;
+return (xtimes);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,3 +2,5 @@
 int _isupper(int c);
 
 int _putchar(char c);
+
+int mul(int a, int b);
