223a20cc0fc1063fcfc5d5a79c320aa53518929d
Mia Ikechukwu <mira.ikechukwu15@gmail.com>
2022-09-15 17:42:25 -0700

    ADded new commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_numbers - it prints the numbers from
+ *
+ *Return: 0
+ */
+void print_numbers(void)
+{
+int t;
+for (t = 0; t <= 9; t++)
+_putchar(t + '0');
+_putchar('\n');
+return;
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,3 +4,5 @@ int _isupper(int c);
 int _putchar(char c);
 
 int mul(int a, int b);
+
+void print_numbers(void);
