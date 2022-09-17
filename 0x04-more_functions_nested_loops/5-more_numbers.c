300a3bcc552fb0d6cdcde9368097cde979b181d6
Mia Ikechukwu <mira.ikechukwu15@gmail.com>
2022-09-16 18:00:37 -0700

    Added new program file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *more_numbers - prints 10 times the number
+ *
+ *Return: 0
+ */
+void more_numbers(void)
+{
+int j;
+int m = 0;
+while (m < 11)
+{
+for (j = 0; j < 15; ++j)
+{
+if (j >= 10)
+_putchar((j / 10) + 48);
+_putchar((j % 10) + 48);
+}
+_putchar('\n');
+m++;
+}
+return;
+}
