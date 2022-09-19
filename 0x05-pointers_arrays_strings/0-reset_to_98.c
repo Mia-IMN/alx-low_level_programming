b2371df518840ecae46e08331747f207063962b7
Mia Ikechukwu <mira.ikechukwu15@gmail.com>
2022-09-19 11:05:31 -0700

    Added new program file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *reset_to_98 - resets value of n
- *@*n - int
- *Return: 0
+ * reset_to_98 - resets value of n
+ * @n: int pointer to reset to 98
+ * Return: 0
  */
 void reset_to_98(int *n)
 {
