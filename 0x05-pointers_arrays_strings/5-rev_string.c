8a4864ca2e05675ff32f5f06929af5113930bfa7
Mia Ikechukwu <mira.ikechukwu15@gmail.com>
2022-09-19 19:14:12 -0700

    Added new program file

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,10 +9,14 @@
 
 void rev_string(char *s)
 {
-int i = 0;
-for (i = strlen(s) - 1; i >= 0; i--)
-{
-_putchar(s[i]);
-}
-_putchar('\n');
-}
+  long int i = sizeof(s);
+  char *p = s;
+while (i >= 0)
+    {
+      *p + (sizeof(s) - i) = s[i];
+     _putchar(s[i]);
+      i--;
+
+    }
+ _putchar('\n');
+ }
