23e58ff70adf3831ad0f32ac61ab0aaf6aae403f
Mia Ikechukwu <mira.ikechukwu15@gmail.com>
2022-09-27 13:52:13 -0700

    Added new program file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ *_strchr - locates a character in a string
+ *@s: char pointer
+ *@c: char pointer
+ *Return: NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+while (*s <= c)
+{
+if (*s == c)
+return (s);
+s++;
+}
+return (NULL);
+}
