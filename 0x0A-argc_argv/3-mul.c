f63bee1588fad991a811049c43c13eda86452d57
Mia Ikechukwu <mira.ikechukwu15@gmail.com>
2022-09-30 17:23:50 -0700

    Debugged last commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,11 @@
 
 int main(int argc, char *argv[])
 {
-int firstnum = atoi(argv[1]);
-int secondnum = atoi(argv[2]);
-int mul = firstnum * secondnum;
-
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-printf("%d\n", mul);
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 return (0);
 }
